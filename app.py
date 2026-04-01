71e3060b5e48e05000f5140aff89a3dabff24c7d
Analice Ferreira <ferreiraanalice122@gmail.com>
2026-04-01 17:05:45 -0300

    Criação da funcionalidade de editar e listar/buscar clientes por nome ou telefone

--- app.py
+++ app.py
@@ -5,9 +5,13 @@ from controller.clientes_controller import cliente_blueprint
 app = Flask(__name__)
 app.secret_key = "chave_mestra"
 
-# Configuração de conexão com o MySQL
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Teste%40123@localhost/crm_faculdade'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# 1. Configuração de conexão com o SQL Server
+app.config['SQLALCHEMY_DATABASE_URI'] = r'mssql+pyodbc://.\SQLEXPRESS01/crm_faculdade?driver=ODBC+Driver+17+for+SQL+Server&trusted_connection=yes&Encrypt=no&TrustServerCertificate=yes'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  
+
+# 2. Configuração de conexão com o MySQL
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Teste%40123@localhost/crm_faculdade'
+#app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Inicializa o SQLAlchemy (ORM)
 db.init_app(app)
